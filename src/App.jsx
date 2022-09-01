b1521a7eee77d7df926c7a479a5b2994efc5c815
kishore-a <csuftitan@LTMacAir-M1-165017.local>
2022-08-31 17:01:59 -0700

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Headers from './components/headers/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Footer from './components/footer/footer'
+const App = () => {
+  return (
+<>
+<Headers/>
+<Nav/>
+<About/>
+<Footer/>
+</>
+    )
+}
+
+export default App
